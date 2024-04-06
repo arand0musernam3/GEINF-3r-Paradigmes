3300a33aa36dd46b2bd72f462f7ac7bdeeed3704
Aniol Juanola <u1978893@campus.udg.edu>
2024-04-06 20:40:58 +0200

    Afegida solució a nonograma.pl i afegit fitxer jocs de proves.

--- TreballProlog/nonograma.pl
+++ TreballProlog/nonograma.pl
@@ -1,40 +1,27 @@
-% consult(predicatsRecomanats). % inclou el fitxer "predicatsRecomanats.pl"
+%rowDef(Hints,Ts,+Len) : Hints es la llista d'indicacions per la fila Ts, que te llargada Len. ¡¡ Ni NHints ni Ts cal que estiguin instanciats !!
+rowDef([],[],0).
 
-nonoPrint(NF,NC,IF,IC,G) :- 
-    countSet(IF,IF_count), countSet(IC,IC_count), 
-    max_list(IF_count,IF_space), max_list(IC_count, IC_space), 
-    preprocessIC(IC_space,IC_count,IC,IC_processed), transpose(IC_processed, IC_printable),
-    printHead(2*IF_space-1,IC_space,IC_printable), 
-    printMult('-',2*IF_space), printMult('-',2*NC-1), print('\n'),
-    preprocessIC(IF_space,IF_count,IF,IF_printable),
-    printBody(IF_printable,G), !.
+rowDef(Res, Ts, Len):-
+    length(Ts,Len),
+    countSublists(Ts,[],Res).
 
-preprocessIC(_,[],[],[]).
-preprocessIC(Space,[This_count|NC_count],[This_IC|IC],[R|Res]) :- This_IC \= [], processColumn(Space,This_count,This_IC,RAux), reverse(RAux,R), preprocessIC(Space,NC_count,IC,Res).
+countSublists([],[],[]).
 
-processColumn(0,0,[],[]).
-processColumn(Size,0,[],[I|Column]) :- Size > 0, I = ' ', NextSize is Size - 1, processColumn(NextSize,0,[],Column).
-processColumn(Size, Length, [H|Hints], [I|Column]) :- Length > 0, I is H, NextSize is Size - 1, NextLength is Length - 1, processColumn(NextSize, NextLength, Hints, Column).
+countSublists([], Sublist, [Count]) :- %CAS ESPECIAL QUAN LA LLISTA ACABA EN 'x'
+    Sublist \= [],  %ens assegurem que almenys hi ha alguna 'x'
+    length(Sublist, Count).
 
-printHead(_,0,[]).
-printHead(InitSpace,NC,[R|Rows]) :- NC > 0, printMult(' ',InitSpace), print('|'), printRow(R), print('\n'), NCnext is NC - 1, printHead(InitSpace,NCnext,Rows).
+countSublists([' '|Ts], [], CountVect) :- %QUAN TROBEM UN ' ' I VENIM D'UN ' '
+    countSublists(Ts, [], CountVect).
 
-countSet([],[]).
-countSet([I|Input],[R|Result]) :- length(I,R), countSet(Input,Result).
+countSublists(['x'|Ts], Sublist, CountVect) :- %TROBEM UNA 'x', ANEM FENT APPEND FINS QUE ARRIBI UN ' ' (cas de sota)
+    append(Sublist, ['x'], NewSublist),
+    countSublists(Ts, NewSublist, CountVect).
 
-printMult(_,0).
-printMult(C,N) :- N > 0, print(C), Nnext is N - 1, printMult(C,Nnext).
-
-printBody([],[]).
-printBody([H|Hints],[R|G]) :- printRow(H), print('|'), printRow(R), print('\n'), printBody(Hints,G).
-
-printRow([]).
-printRow([X|R]) :- print(X), printEnd(R), printRow(R).
-printEnd([]).
-printEnd(_) :- print(' ').
-
-
-
+countSublists([' '|Ts], Sublist, [Count|CountVect]) :- %QUAN TROBEM UN ' ' I VENIM DE UNA 'x'
+    Sublist \= [],
+    length(Sublist, Count),
+    countSublists(Ts, [], CountVect).
 
 %transpose(+Xs,Ys): Ys es la matriu (llista de llistes) Xs transposada. 
 % Assumim que es una matriu ben construida, es a dir, totes les files (subllistes) tenen la mateixa llargada.
@@ -47,3 +34,80 @@ transPrimeraColumna([], [], []). %cas base, la transposada d'una llista buida é
 transPrimeraColumna([[H|T]|Rows], [H|Hs], [T|Ts]) :- transPrimeraColumna(Rows, Hs, Ts). % cas recursiu, agafem el primer element de la primera llista (H), l'afegim al resultat (H|Hs)
                                                                                     % i la resta de la primera llista ho col·loquem a la continuació de la matriu (T|Ts).
                                                                                     % llavors només queda cridar el mateix per a transposar la següent fila de la matriu.
+
+% ============================================ NONOBIDIREC =============================================
+generarFila(_,[],[]).
+generarFila(Len, [Pista|Pistes],[Fila|Files]):-
+    rowDef(Pista, Fila, Len),
+    generarFila(Len, Pistes, Files).
+
+nonoBidirec(NF,NC,IF,IC,G):-
+    generarFila(NC,IF,G),
+    transpose(G,Columnes),
+    generarFila(NF,IC,Columnes).
+
+nonoBidirecPrint(NF,NC,IF,IC,G):-
+    nonoBidirec(NF,NC,IF,IC,G),
+    nonoPrint(NF,NC,IF,IC,G).
+% ======================================================================================================
+
+% ============================================= WRAPPERS ===============================================
+
+solucionarNono(NF,NC,IF,IC,G) :- nonoBidirec(NF,NC,IF,IC,G).
+solucionarNonoPrint(NF,NC,IF,IC,G) :- nonoBidirecPrint(NF,NC,IF,IC,G).
+generarNono(NF,NC,IF,IC,G) :- nonoBidirec(NF,NC,IF,IC,G).
+generarNonoPrint(NF,NC,IF,IC,G) :- nonoBidirecPrint(NF,NC,IF,IC,G).
+
+% ======================================================================================================
+
+% =============================================== PRINT ================================================
+nonoPrint(NF,NC,IF,IC,G) :- 
+    countSet(IF,IF_count), countSet(IC,IC_count), % Compta el nombre de pistes de cada fila i de cada columna respectivament
+    max_list(IF_count,IF_space), max_list(IC_count, IC_space), % Mira quin es el conjunt de pistes més gran a les files i a les columnes
+    preprocessHints(IC_space,IC_count,IC,IC_processed), % Preprocessa les pistes de les columnes, posant espais perquè quedi ben imprès
+    transpose(IC_processed, IC_printable), % proprocessIC retorna les pistes de columnes per columna. Transposa a files per imprimir com s'espera
+    print('\n'), % Imprimeix un salt de línia (en cas de tenir més d'una solució, en prèmer el caràcter 'a' no es produeix un salt de línia automàtic)
+    printHead(2*IF_space-1,IC_printable), % Imprimeix els espais, el separador vertical, i les pistes de columna.
+    printMult('-',2*IF_space), printMult('-',2*NC-1), print('\n'), % Imprimeix el separador horitzontal
+    preprocessHints(IF_space,IF_count,IF,IF_printable), % Preprocessa les pistes de les files, posant espais perquè quedi ben imprès
+    printBody(IF_printable,G), !. % Imprimeix el cos i para quan troba la primera solució
+
+% Preprocessa un conjunt de conjunts de pistes IC (amb subconjunts de llargada NC_count) amb un màxim d'espai Space. Res és el resultat
+preprocessHints(_,[],[],[]).
+preprocessHints(Space,[This_count|NC_count],[This_IC|IC],[R|Res]) :- 
+    This_IC \= [], % Mira que el conjunt no sigui buit
+    reverse(This_IC,This_IC_aux), % Gira l'ordre de les pistes (prerrequisit de processColumn)
+    processColumn(Space,This_count,This_IC_aux,RAux), % Processa
+    reverse(RAux,R), % Gira l'ordre de les pistes AMB els espais (post requisit de processColumn)
+    preprocessHints(Space,NC_count,IC,Res). % Preprocessa la següent
+
+% Retorna Column de mida Size, amb el contingut de Hints (de llargada Length) concatenat d'espais
+processColumn(0,0,[],[]).
+processColumn(Size,0,[],[I|Column]) :- Size > 0, I = ' ', NextSize is Size - 1, processColumn(NextSize,0,[],Column).
+processColumn(Size, Length, [H|Hints], [I|Column]) :- Length > 0, I is H, NextSize is Size - 1, NextLength is Length - 1, processColumn(NextSize, NextLength, Hints, Column).
+
+% Imprimeix les pistes de columnes R deixant InitSpace a la esquerra
+printHead(_,[]).
+printHead(InitSpace,[R|Rows]) :- printMult(' ',InitSpace), print('|'), printRow(R), print('\n'), printHead(InitSpace,Rows).
+    % Espai, separador, pista, següent fila
+
+% I és una llista de n llistes, R és el resultat (una llista amb n llistes on R[n] és la llargada de I[n])
+countSet([],[]).
+countSet([I|Input],[R|Result]) :- length(I,R), countSet(Input,Result).
+
+% Imprimeix N cops C
+printMult(_,0).
+printMult(C,N) :- N > 0, print(C), Nnext is N - 1, printMult(C,Nnext).
+
+% Imprimeix les pistes H de files i la matriu G línia per línia
+printBody([],[]).
+printBody([H|Hints],[R|G]) :- printRow(H), print('|'), printRow(R), print('\n'), printBody(Hints,G). % Pistes, separador, caselles, següent fila
+
+% Imprimeix una fila X posant espais entre caràcters
+printRow([]).
+printRow([X|R]) :- print(X), printSeparator(R), printRow(R).
+
+% Imprimeix un espai si queden caràcters, altrament no imprimeix res
+printSeparator([]).
+printSeparator(_) :- print(' ').
+% ======================================================================================================
